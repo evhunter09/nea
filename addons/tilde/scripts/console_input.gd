3c505d29d16782e6ef618ed2f8ec35127905079e
Evan Amblin <evanamblin@outlook.com>
2026-07-07 11:24:44 +0100

    Sort out plugins (remove tilde)

--- addons/tilde/scripts/console_input.gd
+++
@@ -1,8 +0,0 @@
-extends LineEdit
-
-func _gui_input(event):
-    if event is InputEventKey and event.pressed:
-        if event.keycode == KEY_P:
-            var char = "P" if event.shift_pressed else "p"
-            insert_text_at_caret(char)
-            accept_event()
